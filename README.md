4f7e6b3cd95b84875dd94d2dee9f96b5352dd024
WhenSunset <a1018998632@gmail.com>
2022-04-21 19:50:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# 一键安装Wiki.js
+
+## 一、前提
+
+- 1.确保 Docker 已经安装，并且已经处于运行状态
+- 2.确保网络顺畅，能够访问 Github
+
+
+## 二、使用
+
+### 1.下载数据
+
+- 方法1：`git clone ssh://git@192.168.123.204:4422/Docker/wiki-js-deploy.git`
+- 方法2：[进入这里手动下载解压](http://gitlab.heshixi.loc/Docker/wiki-js-deploy)
+
+### 2.部署
+
+- Linux：
+```shell
+## 第一步，进入刚刚的下载下来数据的
+
+## 第二步，运行
+bash run-linux.sh
+
+## 第三步，按照提示输入
+```
+- Mac：
+
+```shell
+## 第一步，进入刚刚的下载下来数据的
+
+## 第二步，运行
+sh run-mac.sh
+
+## 第三步，按照提示输入
+```
---
+++ run-mac.sh
@@ -0,0 +1,30 @@
+#/bin/bash
+
+main() {
+    echo ">>>>>>>> 此脚本用于 Mac 系统，请确认系统的正确，如果确认则忽略本提示。"
+    echo ">>>>>>>> 输入期望部署的路径(输入 default 表示使用默认路径：~/.Wiki-WS): "
+    read path 
+    if [ $path == "default" ]
+    then
+        path="$HOME/.Wiki-W" 
+        echo ">>>>>>>> 部署路径设置为默认的：$path，如果部署出现异常请删除该目录，然后重新执行部署流程。"
+    else
+        echo ">>>>>>>> 部署路径设置为：$path，如果部署出现异常请删除该目录，然后重新执行部署流程。" 
+    fi
+    mkdir -p $path/wiki-data 
+    unzip -d $path/wiki-data wiki-data.zip
+    prepareWiki 
+    docker-compose -f $path/docker-compose.yml up -d
+    echo ">>>>>>>> Wiki.js成功运行"
+}
+
+prepareWiki() {
+    echo ">>>>>>>> 删除已存在的Wiki.js"
+    sudo docker stop heshixi-wiki-js-default-deploy && docker rm heshixi-wiki-js-default-deploy && docker stop heshixi-wiki-js-db-default-deploy && docker rm heshixi-wiki-js-db-default-deploy 
+    echo ">>>>>>>> 开始加载Wiki.js"
+    sudo docker load --input postgres.tar
+    sudo docker load --input wiki.tar
+    echo ">>>>>>>> 开始运行Wiki.js"
+}
+
+main
